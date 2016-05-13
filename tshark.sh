4ac120ef273bb7c6b508dbc7ca94b63e39b06d9d
palanthas <dmscholten@gmail.com>
2016-05-13 19:31:08 -0400

    added tshark.sh

---
+++ tshark.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir -p "$HOME/Desktop/tmp"
+PIDFILE="$HOME/Desktop/tmp/myprogram.pid"
+
+if [ -e "${PIDFILE}" ] && (ps -u $(whoami) -opid= |
+                           grep -P "^\s*$(cat ${PIDFILE})$" &> /dev/null); then
+  echo "Already running."
+  exit 99
+fi
+
+#tshark -i eth0 -b duration:3600 -b files:5 -w $HOME/Desktop/2016-05-13.pcap &
+tshark -i eth0 -b duration:5 -b files:5 -w $HOME/Desktop/2016-05-13.pcap &
+
+echo $! > "${PIDFILE}"
+chmod 644 "${PIDFILE}"
