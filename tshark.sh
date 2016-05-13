7d4ae28da5f522b516b2d42f8bd0dc61bcea93b1
palanthas <dmscholten@gmail.com>
2016-05-13 19:58:30 -0400

    Added comments and changed duration time to 1 hour. Removed unused line.

--- tshark.sh
+++ tshark.sh
@@ -3,14 +3,15 @@
 mkdir -p "$HOME/Desktop/tmp"
 PIDFILE="$HOME/Desktop/tmp/tshark.pid"
 
+# Check if tshark is already running. If not, start tshark
 if [ -e "${PIDFILE}" ] && (ps -u $(whoami) -opid= |
                            grep -P "^\s*$(cat ${PIDFILE})$" &> /dev/null); then
   echo "Already running."
   exit 99
 fi
 
-#tshark -i eth0 -b duration:3600 -b files:5 -w $HOME/Desktop/2016-05-13.pcap &
-tshark -i eth0 -b duration:5 -b files:5 -w $HOME/Desktop/tshark.pcap &
+# Run tshark in ring buffer mode. Creates 24 files in 1 hour increments. After 24 files it will start writing over the oldest files to keep a roll 24 hour log of netowrk activity.
+tshark -i eth0 -b duration:3600 -b files:24 -w $HOME/Desktop/tshark.pcap &
 
 echo $! > "${PIDFILE}"
 chmod 644 "${PIDFILE}"
